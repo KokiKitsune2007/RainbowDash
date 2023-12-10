301634e6ad2f5f0dc836a915464a55a5619b8db4
Koki <128624869+KokiKitsune2007@users.noreply.github.com>
2023-12-09 22:49:27 -0800

    Update main.c

--- Source/main.c
+++ Source/main.c
@@ -1,1 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <asndlib.h>
+#include <tremor/ivorbiscodec.h>
+#include <tremor/ivorbisfile.h>
+#include <gccore.h>
+#include <unistd.h>
+#include <string.h>
 
+#include "oggplayer.h"
